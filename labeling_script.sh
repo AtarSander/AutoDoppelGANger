fab2588765c5de78803194b2883f840834c1a9be
mrozek <01178582@pw.edu.pl>
2024-04-22 23:37:34 +0200

    IMPLEMENTATION: Implemented script that labels cut photos

---
+++ labeling_script.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+TERMINAL_WINDOW_ID=$(xdotool getwindowfocus)
+
+for file in ./dataset/car_cut/*; do
+    echo "File: $file"
+    echo "Click 'f' to move file to front, 'b' to back, 's' to side, 'q' to quit"
+
+
+    feh -g 800x600 -x "$file" &
+
+    FEH_PID=$!
+
+    sleep 0.5
+
+    xdotool windowfocus $TERMINAL_WINDOW_ID
+
+    read -n 1 key
+
+    kill $FEH_PID
+
+    case $key in
+        f)
+            mv "$file" ./datasets_labeled/front/
+            ;;
+        b)
+            mv "$file" ./datasets_labeled/back/
+            ;;
+        t)
+            rm "$file" 
+            ;;
+        s)
+            mv "$file" ./datasets_labeled/side/
+            ;;
+        q)
+            exit 0
+            ;;
+        *)
+            echo "Unknown input: $key"
+            ;;
+    esac
+done
