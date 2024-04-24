a92c0aeb16d0bddc0335e750c1bab06c72b9ee94
atarsander <aleksander.szymczyk.stud@pw.edu.pl>
2024-04-24 13:43:01 +0200

    Created data augmenter class for multipling dataset image.
    - implemented flipping, rotating, brightness and contrast adjusting and gaussian noise application functions
    - augment_dataset function iterates through a folder and creates 6 more variations of each image

--- src/data_augmenter.py
+++ src/data_augmenter.py
@@ -1,12 +1,32 @@
 import cv2
+import os
 import numpy as np
+from src.progress_bar import ProgressBar
 
 class DataAugmenter:
-    def create_variations(self, image_path):
-        image = cv2.imread(image_path)
+    def __init__(self, directory):
+        self.directory = directory
+        self.progress_bar = ProgressBar()
 
-        mean_pixel_value = np.mean(image)
-        adjusted_image = (image - mean_pixel_value) * contrast_factor + mean_pixel_value
+    def augment_dataset(self):
+        filenames = os.listdir(self.directory)
+        self.progress_bar.start(len(filenames))
+        for f_id, filename in enumerate(filenames):
+            augmented_images = self.create_variations(filename)
+            self.progress_bar.update(100, f_id)
+            for new_filename, image in augmented_images.items():
+                self.save_image(image, new_filename)
+
+    def create_variations(self, filename):
+        augmented_images = {}
+        image = cv2.imread(self.directory+"/"+filename)
+        augmented_images[self.set_filename("flip", filename)] = self.filp_horizontally(image)
+        augmented_images[self.set_filename("-rotate", filename)] = self.rotate_image(image, -5)
+        augmented_images[self.set_filename("+rotate", filename)] = self.rotate_image(image, 5)
+        augmented_images[self.set_filename("contrast", filename)] = self.adjust_contrast(image, 1.4)
+        augmented_images[self.set_filename("brightness", filename)] = self.adjust_brightness(image, 0.2)
+        augmented_images[self.set_filename("gaussian", filename)] = self.add_gaussian_noise(image, 0, 3)
+        return augmented_images
 
     def filp_horizontally(self, image):
         image_flipped_h = cv2.flip(image, 1)
@@ -19,16 +39,45 @@ class DataAugmenter:
         return image_rotated
 
     def adjust_contrast(self, image, contrast_factor):
-        mean_pixel_value = np.mean(image)
         image_float = image.astype(np.float32) / 255.0
-        adjusted_image = (image_float - mean_pixel_value) * contrast_factor
-        adjusted_image = np.clip(adjusted_image, 0, 1)
-        adjusted_image = (adjusted_image * 255).astype(np.uint8)
-        return adjusted_image
+        adjusted_image = (image_float) * contrast_factor
+        return self.reformat_to_int(adjusted_image)
 
     def adjust_brightness(self, image, brightness_factor):
         image_float = image.astype(np.float32) / 255.0
         adjusted_image = image_float + brightness_factor
+        return self.reformat_to_int(adjusted_image)
+    
+    def add_gaussian_noise(self, image, mean, sigma, scale_factor = 0.02):
+        random_noise = np.random.normal(mean, sigma, image.shape)
+        image_float = image.astype(np.float32) /255.0
+        adjusted_image = image_float + random_noise * scale_factor
+        return self.reformat_to_int(adjusted_image)
+    
+    def reformat_to_int(self, adjusted_image):
         adjusted_image = np.clip(adjusted_image, 0, 1)
         adjusted_image = (adjusted_image * 255).astype(np.uint8)
         return adjusted_image
+    
+    def set_filename(self, type, org_filename):
+        tmp = org_filename.rstrip(".jpg")
+
+        if type=='flip':
+            tmp+= 'f'
+        elif type=='-rotate':
+            tmp+='-r'
+        elif type=='+rotate':
+            tmp+='+r'
+        elif type== 'contrast':
+            tmp+='c'
+        elif type=='brightness':
+            tmp+='b'
+        elif type=='gaussian':
+            tmp+='g'
+
+        filename = tmp+ '.jpg'
+        return filename
+
+    def save_image(self, image, filename):
+        dir_path = self.directory + filename
+        cv2.imwrite(dir_path, image)
