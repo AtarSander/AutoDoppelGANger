03f7b6a2aa23ec4d6609feaf26215066fc8acc4c
AtarSander <szymczyk.alek1@gmail.com>
2024-04-24 17:21:28 +0200

    Added directory for log keeping, deleted unnecessary code in labeler.
    - logs directory consists of fake and real subdir for tensorboard log keeping

--- src/labeler.py
+++ src/labeler.py
@@ -10,19 +10,6 @@ class Labeler:
         for filename in filenames:
             filepath = os.path.join(directory, filename)
             print(filename)
-            x = [1, 2, 3, 4, 5]
-            y = [2, 3, 5, 7, 11]
-
-            # Create plot
-            plt.plot(x, y)
-
-            # Add labels and title
-            plt.xlabel('X-axis')
-            plt.ylabel('Y-axis')
-            plt.title('Example Plot')
-
-            # Display plot
-            plt.show()
             self.print_image(filepath, filename)
             self.label_file(filename, filepath)
 
