a30071a03ddcae31ad94e4587f6e02b35cf217e6
mrozek <01178582@pw.edu.pl>
2024-04-25 11:14:25 +0200

    IMPLEMENTATION: Created computind FID and Inception in shell

--- src/autodoppelganger_shell.py
+++ src/autodoppelganger_shell.py
@@ -1,7 +1,9 @@
-from data_preprocessor import DataPreprocessor
-from car_cutter import CarCutter
+from .data_preprocessor import DataPreprocessor
+from .car_cutter import CarCutter
 from nuimages import NuImages
-from gan import GAN
+from .gan import GAN
+from .model_eval import ModelEval 
+from matplotlib import pyplot as plt
 import torch
 import json
 
@@ -10,14 +12,16 @@ class AutoDoppelGANgerShell:
         print("Welcome to AutoDoppelGANger. Type 'help' to list commands.")
 
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.model = GAN(64, 64, 3, 100, device, '../logs')
-        self.dataset = None
+        self.model = GAN(64, 64, 3, 100, device)
+        self.model_eval = ModelEval(32, device)
 
     def help(self):
         print("Available commands:")
         print("lddst - Load dataset")
-        print("train - Start training the model")
-        print("eval - Evaluate the model")
+        print("gensam - Generate Samples (Usage: gensam <num_samples> <num_rows> <num_columns>)")
+        print("train - Start training the model (Usage: train <path to json file>)")
+        print("incsc - Calculate inception score of the model")
+        print("fid - Calculate FID of the model")
         print("exit - Exit the shell")
 
     def cut_nu_images(self, dataroot, out_path, version, min_size_x, min_size_y):
@@ -42,13 +46,32 @@ class AutoDoppelGANgerShell:
         self.model.train(self.dataset, num_epochs, batch_size, learning_rate,
                          beta1, beta2, time_limit)
         if save_weights:
-            self.model.save_models_weights("../models/checkpoints")
+            self.model.save_models_weigths(
+                    "/home/mrozek/AutoDoppelGANger/models/checkpoints/discriminator.pth", "/home/mrozek/AutoDoppelGANger/models/checkpoints/generator.pth")
         print("Done.")
 
+    def generate_samples(self, num_samples, num_rows, num_cols):
+        print("Displaying images...")
+        images = self.model.generate_samples(num_samples)
+        _, axes = plt.subplots(num_rows, num_cols, figsize=(10, 10))
+        for i, ax in enumerate(axes.flat):
+            ax.imshow(images[i].permute(1, 2, 0).clamp(0, 1))
+            ax.axis('off')
+        plt.subplots_adjust(wspace=0.1, hspace=0.1)
+        plt.show()
+        print("Done...")
 
-    def eval(self):
-        print("Evaluating model...")
-        # Add evaluation logic here
+    def display_inception_score(self):
+        print("Calculating Inception Score...")
+        generated_images = self.model.generate_samples(1024)
+        inc_score = self.model_eval.compute_inception_score(generated_images)
+        print(f"Inception score of GAN model: {inc_score:.4f}")
+        
+    def display_FID(self):
+        print("Calculating FID...")
+        generated_images = self.model.generate_samples(1024)
+        FID_score = self.model_eval.compute_FID(self.model.loaded_data, generated_images)
+        print(f"Inception score of GAN model: {FID_score:.4f}")
 
     def exit(self):
         print("Exiting...")
@@ -71,8 +94,14 @@ class AutoDoppelGANgerShell:
                                training_setup["batch_size"],
                                training_setup["learning_rate"],
                                training_setup["save_weights"])
-                elif parts[0] == 'eval':
-                    self.eval()
+                elif parts[0] == 'ldwghts':
+                    self.model.load_model_weights(parts[1], parts[2])
+                elif parts[0] == 'gensam':
+                    self.generate_samples(parts[1], parts[2], parts[3])
+                elif parts[0] == 'fid':
+                    self.display_FID()
+                elif parts[0] == 'incsc':
+                    self.display_inception_score()
                 elif parts[0] == 'exit':
                     running = self.exit()
                 else:
